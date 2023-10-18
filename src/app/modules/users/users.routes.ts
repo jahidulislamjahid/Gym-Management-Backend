1062d2044c5bedaa30250e51dfd0a10bb6da25d4
jahid <info.jahidulislamjahid@gmail.com>
2023-10-19 03:01:15 +0600

    user module polish

--- src/app/modules/users/users.routes.ts
+++ src/app/modules/users/users.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { userRole } from '@prisma/client';
+import auth from '../../middlewares/auth';
 import { UserController } from './users.controller';
 
 const router = express.Router();
@@ -6,6 +8,22 @@ const router = express.Router();
 router.get(
   '/',
   UserController.getAllUsersController
+);
+
+router.get(
+  '/:userId',
+  UserController.getSingleUser
+);
+
+router.get(
+  '/my-profile/:userId',
+  auth(userRole.ADMIN, userRole.SUPER_ADMIN, userRole.USER, userRole.DOCTOR),
+  UserController.getMyProfile
+);
+
+router.patch(
+  '/update-profile/:profileId',
+  UserController.updateProfileInfo
 );
 
 export const UserRoutes = router;
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,6 +1,9 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import { IProfileUpdateRequest } from './users.interface';
 
 const getAllUserService = async (options: IPaginationOptions): Promise<any> => {
   const { limit, page, skip } = paginationHelpers.calculatePagination(options);
@@ -31,6 +34,127 @@ const getAllUserService = async (options: IPaginationOptions): Promise<any> => {
   };
 };
 
+const getSingleUser = async (userId: string): Promise<any | null> => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!existingUser) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not Found !!');
+  }
+
+  const result = await prisma.user.findUnique({
+    where: {
+      userId,
+    },
+    select: {
+      userId: true,
+      email: true,
+      profile: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not Found !!');
+  }
+
+  return result;
+};
+
+const updateProfileInfo = async (
+  profileId: string,
+  payload: IProfileUpdateRequest
+): Promise<{
+  message: string;
+  updatedInfo: IProfileUpdateRequest;
+}> => {
+  if ('profileId' in payload) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Profile ID cannot be changed');
+  }
+
+  const existingProfile = await prisma.profile.findUnique({
+    where: {
+      profileId,
+    },
+  });
+
+  if (!existingProfile) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Profile not found!');
+  }
+
+  const { firstName, lastName, profileImage, role } = payload;
+
+  const updateData: Partial<IProfileUpdateRequest> = {};
+
+  if (firstName !== undefined) {
+    updateData.firstName = firstName;
+  }
+
+  if (lastName !== undefined) {
+    updateData.lastName = lastName;
+  }
+
+  if (profileImage !== undefined) {
+    updateData.profileImage = profileImage;
+  }
+
+  if (role !== undefined) {
+    updateData.role = role;
+  }
+
+  if (Object.keys(updateData).length === 0) {
+    return {
+      message: 'No changes to update',
+      updatedInfo: {},
+    };
+  }
+
+  const result = await prisma.profile.update({
+    where: {
+      profileId,
+    },
+    data: updateData,
+  });
+
+  if (!result) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Profile update failed');
+  }
+
+  return {
+    message: 'Profile information updated successfully',
+    updatedInfo: updateData,
+  };
+};
+
+const getMyProfile = async (userId: string): Promise<any | null> => {
+  console.log('userId', userId);
+  const result = await prisma.user.findUnique({
+    where: {
+      userId,
+    },
+    select: {
+      userId: true,
+      email: true,
+      profile: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not Found !!');
+  }
+
+  return result;
+};
+
 export const UserService = {
   getAllUserService,
+  getSingleUser,
+  updateProfileInfo,
+  getMyProfile,
 };
